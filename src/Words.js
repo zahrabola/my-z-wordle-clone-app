09de0bfc391b619da0fcfb12de89f3a1db53a1e4
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-09-26 22:39:15 +0100

    Update

---
+++ src/Words.js
@@ -0,0 +1,10 @@
+export const defaultwordleboard = [
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+    ["","","","",""],
+]
+// empty letters like the board,  - one array represent a new attempt
+// board will represent each letter on each cell of the board and new attempt 
