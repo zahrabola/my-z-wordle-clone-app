10c5fa999cc48fed8953bf7d476b69c5983bf0f7
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-09-28 14:46:14 +0100

    update

--- src/Components/Letter.js
+++ src/Components/Letter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Letter = ({letterPos, attemptVal}) => {
+    const letter = board[attemptVal][letterPos]
     return (
         <div className='letter'>
-            letter
+            {letter}
         </div>
     );
 }
 
 export default Letter;
+// attemptVal - value
+// letterpos = letter postion 
