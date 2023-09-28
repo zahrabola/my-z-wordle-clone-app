d53a0aedf7357fca7bd7e514355b03f1d3f1802d
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-09-28 15:33:48 +0100

    update

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Key = () => {
+const Key = ({keyValue, bigKey}) => {
     return (
-        <div>
-            key
+        <div className='key' id={bigKey && "big"}>
+        {keyValue}
         </div>
     );
 }
 
 export default Key;
+//taking in the value of the key through props
+// conditional logic for the enter and delte
