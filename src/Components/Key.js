90317fd87c6f50540d57ddffb515cf35efae5870
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-09-30 16:13:24 +0100

    Update
    function of button

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -10,6 +10,13 @@ const Key = ({keyValue, bigKey}) => {
 if (keyValue === "Enter") {
 if (currentAttempt.letterPos !== 5) return;
 setCurrentAttempt ({attempt : currentAttempt.attempt + 1, letterPos: 0 });
+} else if (keyValue === "Delete"){
+    if (currentAttempt.letterPos === 0) return;
+    const currrentNewBoard = [...board]
+    currrentNewBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
+    setBoard(currrentNewBoard)
+    setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos -1 })
+
 } else {
     if(currentAttempt.letterPos > 4) return;
     const currrentNewBoard = [...board]
@@ -30,4 +37,5 @@ export default Key;
 //taking in the value of the key through props
 // conditional logic for the enter and delte
 //conditional rendering if statements greater than 4 return a press enter
-//logic inside enter
+//logic inside enter - enter functionality 
+// delete funcutionality 
