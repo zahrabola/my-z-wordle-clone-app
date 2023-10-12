ea7ccf4e0c3bae1de33a1042d6caaf7892a99a1c
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-10-12 21:51:16 +0100

    update

--- src/Components/Words.js
+++ src/Components/Words.js
@@ -1,3 +1,4 @@
+import wordbank from '../wordle-bank.txt'
 export const defaultwordleboard = [
     ["","","","",""],
     ["","","","",""],
@@ -6,5 +7,19 @@ export const defaultwordleboard = [
     ["","","","",""],
     ["","","","",""],
 ]
+
+
+export const wordGenerateSet = async () => {
+let wordSet;
+await fetch(wordbank).then((response) => response.text())
+.then((result) => {
+console.log(result)
+const wordArray = result.split("\n")
+wordSet = new Set (wordArray)
+})
+return { wordSet}
+};
 // empty letters like the board,  - one array represent a new attempt
 // board will represent each letter on each cell of the board and new attempt 
+// creating a function generating word set 
+// sets in javascript - check if the words exists 
